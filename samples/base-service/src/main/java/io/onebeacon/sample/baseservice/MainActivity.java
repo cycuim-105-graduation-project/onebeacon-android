6362c96edcc6d2e6e313c5501899ab84b61570e6
pacy <mnet2886@gmail.com>
2016-11-23 22:01:42 +0800

    Add Bluetooth detection

--- samples/base-service/src/main/java/io/onebeacon/sample/baseservice/MainActivity.java
+++ samples/base-service/src/main/java/io/onebeacon/sample/baseservice/MainActivity.java
@@ -1,16 +1,21 @@
 package io.onebeacon.sample.baseservice;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
 
 import io.onebeacon.api.OneBeacon;
 import io.onebeacon.api.ScanStrategy;
 
 public class MainActivity extends ActionBarActivity implements ServiceConnection {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
     private MonitorService mService = null;
 
     @Override
@@ -18,6 +23,24 @@ public class MainActivity extends ActionBarActivity implements ServiceConnection
         super.onCreate(savedInstanceState);
         if (!bindService(new Intent(this, MonitorService.class), this, BIND_AUTO_CREATE)) {
             setTitle("Bind failed! Manifest?");
+        }
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // 檢查是否有可用的藍牙裝置
+        if (mBluetoothAdapter == null) {
+            // 若無可用裝置時執行
+            Toast.makeText(this, "Bluetooth not supported on this Device", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        // 如果藍牙目前不可用，請求使用者開啟藍芽功能。
+        if (!mBluetoothAdapter.isEnabled()) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
         }
     }
 
@@ -61,5 +84,19 @@ public class MainActivity extends ActionBarActivity implements ServiceConnection
         // Activity is not in foreground, make a trade-off between battery usage and scan latency
         OneBeacon.setScanStrategy(ScanStrategy.BALANCED);
         super.onPause();
+    }
+
+    // 使用onActivityResult 接收其他 Activity回傳的資料
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // 接收請求開啟藍芽功能的結果
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            // 使用者授權藍牙後執行
+            Toast.makeText(this, "使用者已授權藍牙使用", Toast.LENGTH_SHORT).show();
+        } else if (requestCode == 1 && resultCode == RESULT_CANCELED) {
+            // 使用者拒絕授權藍牙後執行
+            Toast.makeText(this, "使用者拒絕授權藍牙使用", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
     }
 }
